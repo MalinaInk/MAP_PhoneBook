cfcaf74ca246dc2d5d22ee7181ad88eda1f6c8a0
mo <nataleshen.ko@yandex.ru>
2022-11-23 12:28:09 +0300

    commit-2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Map;
+import java.util.Random;
+
 public class Main {
+    private final static Random RANDOM = new Random();
     public static void main(String[] args) {
-        System.out.println("PhoneBook!");
+        PhoneBook phoneBook = new PhoneBook();
+        while (phoneBook.getEntries().size() < 20) {
+            phoneBook.addPhone(getRandomName(), getPhoneNumber());
+        }
+        for (Map.Entry<FullName, String> entry : phoneBook.getEntries()) {
+            System.out.printf("%s - %s%n", entry.getKey(), entry.getValue());
+            }
+       }
+
+    public static FullName getRandomName() {
+        String name = "Test" + RANDOM.nextInt(1_000);
+        String lastName = "TestLast" + RANDOM.nextInt(1_000);
+        return new FullName(name, lastName);
+    }
+
+    public static String getPhoneNumber() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 11; i++) {
+            stringBuilder.append(RANDOM.nextInt(10));
+        }
+        return stringBuilder.toString();
     }
 }
---
+++ src/PhoneBook.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class PhoneBook {
+    private final Map<FullName, String> book = new HashMap<>();
+
+    public void addPhone(FullName fullName, String phone) {
+        book.put(fullName, phone);
+    }
+
+    public Set<Map.Entry<FullName, String>> getEntries() {
+        return  book.entrySet();
+    }
+    //сеттер через entrySet, чтобы не показывать наружу внутреннюю логику и состояние  изменяемого объекта
+}
